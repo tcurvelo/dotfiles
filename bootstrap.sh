4a8165e40a635db938e9e14a163113a1efc25551
Thiago Curvelo <tcurvelo@gmail.com>
2012-11-17 16:21:50 -0300

    use rsync in bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-cd $(dirname "$0")
-
-FILES=$(ls -a | egrep -v '(^.$|^..$|bootstrap.sh|.git|README.rst)')
-
-echo $FILES
-
-if [ "$FILES" != "" ] 
-then 
-	cp -r -s $FILES ~/
-fi
+rsync -av \
+      --exclude '.git/' --exclude 'bootstrap.sh' --exclude 'README.rst' \
+      $(dirname "$0")  ~/
 
