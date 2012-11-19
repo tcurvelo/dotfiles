15193b376b3cf94cf5b15b9cb38fbf0e4a356a17
Thiago Curvelo <tcurvelo@gmail.com>
2012-11-19 08:38:06 -0300

    update webchat .js script, to do some css changes

--- .js/webchat.freenode.net.js
+++ .js/webchat.freenode.net.js
@@ -1,16 +1,17 @@
 
-alert('it works!');
+alert('it works!?');
 
-/*
-.qwebirc-qui .nicklist, .qwebirc-qui .lines, .qwebirc-qui .ircwindow div {
-    font-family: "Ubuntu Mono", Monaco, "Lucida Console", monospace;
-    background-color: #FFF; 
-}
 
-.qwebirc .colourline .Xc4, .Xc4 + span {
-    color: #DDD;
-    font-size: 75%;
-    font-style: italic;
-}
-*/
- 
+$(function(){
+    $("head").append(
+        '<!-- .js style -->\n<style>\n\
+        .qwebirc-qui .nicklist, .qwebirc-qui .lines, .qwebirc-qui .ircwindow div {\n\
+        font-family: "Ubuntu Mono", Monaco, "Lucida Console", monospace;\n\
+        background-color: #FFF;\n\
+        }\n\
+    .qwebirc .colourline .Xc4, .Xc4 + span {\n\
+        color: #DDD;\n\
+        font-size: 75%;\n\
+        font-style: italic\n\
+    }</style>');
+});
