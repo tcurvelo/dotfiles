483852dedaa6c139738f20f0014ef04810ce7362
Thiago Curvelo <tcurvelo@gmail.com>
2013-04-29 16:34:48 -0300

    Buildout's cache dirs
    * Move cache dirs to /var/cache, in order to encrypt home folder
    * Create /var/cache/buildout{dlcache, eggs} if nonexistent

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,12 @@ rsync -av \
       --exclude '.git/' --exclude 'bootstrap.sh' --exclude 'README.rst' \
       $(dirname "$0")  ~/
 
+
+#Create buildout cache directories
+if [ ! -d /var/cache/buildout/eggs ] || [ ! -d /var/cache/buildout/dlcache ]
+then 
+    sudo mkdir -p /var/cache/buildout/eggs
+    sudo mkdir -p /var/cache/buildout/dlcache
+    sudo -E chown -R $USER /var/cache/buildout/ -R
+fi
+
