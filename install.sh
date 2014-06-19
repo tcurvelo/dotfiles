15f4ab9dfa89077f7827b674d1a7ab6f8895d5f3
Thiago Curvelo <tcurvelo@gmail.com>
2014-06-18 21:59:28 -0300

    fix rsync parameters

--- install.sh
+++ install.sh
@@ -3,12 +3,14 @@
 git submodule init
 git submodule update
 
+context=$(dirname $(readlink -f "$0"))
+
 rsync -av \
   --exclude '.git/' \
   --exclude '.gitignore' \
   --exclude 'install.sh' \
   --exclude 'README.rst' \
-  "$(dirname $(readlink -f \"$0\"))" ~/
+  "$context"/ ~/
 
 vim +BundleInstall +qall
 
