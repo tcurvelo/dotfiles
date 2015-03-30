fb5d7e81aa0daa94fb97dfbd68d28180ea45cd3f
Thiago Curvelo <tcurvelo@gmail.com>
2015-03-30 14:11:10 -0300

    Use git-subtree instead git-submodules

--- install.sh
+++ install.sh
@@ -3,9 +3,13 @@
 context="$(dirname "$(readlink -f "$0")")"
 
 cd "$context"
-git submodule init
-git submodule update
-git submodule foreach git pull origin master
+
+for subtree in ".oh-my-zsh https://github.com/robbyrussell/oh-my-zsh.git master" \
+               ".vim/bundle/neobundle.vim https://github.com/Shougo/neobundle.vim master"
+do
+  git subtree pull --prefix $subtree --squash || git subtree add --prefix $subtree --squash
+done
+
 cd -
 
 rsync -av \
