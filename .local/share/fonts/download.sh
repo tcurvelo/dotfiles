ed75f4690cac366a2bd93fa6607c38029ac3ade0
Thiago Curvelo <tcurvelo@gmail.com>
2018-07-13 22:37:45 -0300

    Update fonts

--- .local/share/fonts/download.sh
+++ .local/share/fonts/download.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+os=$(uname -s)
+case "$os" in
+  Linux*)     dest="~/.local/share/fonts";;
+  Darwin*)    dest="/Library/Fonts";;
+esac
+
+
 context=$(dirname $0)
 file="FiraCode.zip"
 
 wget \
   -cq --no-check-certificate --directory-prefix=$context \
-  "https://github.com/ryanoasis/nerd-fonts/releases/download/v1.2.0/FiraCode.zip" && \
-    unzip -x $context/$file -d $context
-
+  https://github.com/ryanoasis/nerd-fonts/releases/download/v2.0.0/FiraCode.zip \
+  && unzip \
+     $context/$file \
+     Fura\ Code\ {Bold,Light,Medium,Regular,Retina}\ Nerd\ Font\ Complete.ttf \
+     -d $dest
